9e439e790bce3e8200c0d214360b6d0aa99018a2
Kanksha-09 <145252325+Kanksha-09@users.noreply.github.com>
2024-07-06 14:07:13 +0000

    conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+//let score = "33"
+
+let score = "33ab"
+
+//let score = null // => 0
+// let score = undefined // =>NaN
+
+// true => 1 and false => 0
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valInNo = Number(score)
+console.log(typeof valInNo);
+console.log(valInNo);
+
+
+
+let isLoggedIn = "kanksha"
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn);
+// 1 => true ; 0 => false 
+// "" => false 
+// kanksha => true
+
