610c24f5b2824550d2b9b2b88e0684108f2179f8
Kanksha-09 <145252325+Kanksha-09@users.noreply.github.com>
2024-07-07 05:38:56 +0000

    comparisions done

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2" > 1); //automatically converts string into integer
+
+console.log("02" > 1); //gives the same result as above
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+                            //avoid these comparisions
+console.log(undefined ==0);
+console.log(undefined >=0);
+console.log(undefined > 0);
+
+// ===
+
+console.log("2" === 2);
+
