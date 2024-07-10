c7ff72e021c782783ca04bedc646139fc2734308
Kanksha-09 <145252325+Kanksha-09@users.noreply.github.com>
2024-07-10 19:20:54 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,59 @@
+function saymyname(){
+    console.log("Kanksha");
+}
+
+// saymyname()
+
+// function addnos(no1,no2){
+//     console.log( no1+no2);
+// }
+
+function addnos(no1,no2){
+    // let result = no1+no2
+    // return result;
+
+    return no1+no2;
+}
+
+// addnos(3,4)
+// addnos(3,null)
+// addnos(3,"a")
+
+const result=addnos(3,5)
+// console.log(result);
+
+// function loginUser(username = "sam"){ //default value is sam
+//     if(!username){
+//         console.log("Please enter a valid name");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+function loginUser(username){
+    if(!username){
+        console.log("Please enter a valid name");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUser("Kanksha"));
+
+// console.log(loginUser()); // => o/p= undefined just logged in
+
+function calculatCartPrice(val1,val2, ...num1){
+    return num1;
+}
+
+console.log(calculatCartPrice(2,4,5));
+
+const user ={
+    username:"kanksha",
+    price: "infinity"
+}
+
+function handleObj(anyobj){
+    console.log(`Username is ${anyobj.username} and the price is ${anyobj.price}`);
+}
+
+handleObj(user)
